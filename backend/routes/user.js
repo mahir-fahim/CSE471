3755eaf0750716f6c158f8d4a52250f0ee8d6893
moriyamriana235-359 <moriyam.akter.riana@g.bracu.ac.bd>
2025-04-23 11:30:47 +0600

    Add files via upload

---
+++ backend/routes/user.js
@@ -0,0 +1,16 @@
+router.put("/profile/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      $set: {
+        weight: req.body.weight,
+        height: req.body.height,
+        fitnessLevel: req.body.fitnessLevel,
+        foodHabit: req.body.foodHabit,
+      },
+    }, { new: true });
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
