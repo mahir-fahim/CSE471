b15559774a59852711640054881ee6713a137c26
moriyamriana235-359 <moriyam.akter.riana@g.bracu.ac.bd>
2025-04-23 11:43:40 +0600

    Add files via upload

---
+++ backend/models/User.js
@@ -0,0 +1,21 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  // Add new fields here
+  weight: Number,
+  height: Number,
+  fitnessLevel: {
+    type: String,
+    enum: ["Beginner", "Intermediate", "Advanced"],
+  },
+  foodHabit: {
+    type: String,
+    enum: ["Vegetarian", "Non-Vegetarian", "Vegan"],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
